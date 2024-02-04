c79617ed44d329d092216e74bf8988f917d8d659
Amina-Ibm <aminaibm01@gmail.com>
2024-02-04 21:38:24 +0400

    added background image and changed font colour

--- src/index.js
+++ src/index.js
@@ -10,17 +10,29 @@ function addCity(event) {
 function searchCity(city) {
     let apiKey = "64f96a0b2d38108e7c3432831d7cf1ae";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(addTemperature);    
+    let forecastUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    console.log(forecastUrl);
+    axios.get(apiUrl).then(addTemperature); 
+    axios.get(forecastUrl).then(addforecast); 
 }
 
-function addTime(date){
-    days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let hours = date.getHours;
-    let minutes = date.getMinutes;
-
-    let day = days[date.getDay];
+function addforecast(response){
+    let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
     
+    let forecastHtml = "";
+    days.forEach(function(day){
+        let forecast = `<div class="col-2">
+    <span id="day">${day}</span>
+    <img src="http://openweathermap.org/img/wn/10n@2x.png" width="40px">
+    <div id="temps">
+        <span id="max-temp">${response.data.list.main.temp_max}°C</span>
+        <span id="min-temp">${response.data.list.main.temp_min}°C</span>
+    </div></div>`;
+        forecastHtml = forecastHtml + forecast;
+    });
+    console.log(response.data.list[0].main.temp_max);
+    let forecastDisplay = document.querySelector("#forecast");
+    forecastDisplay.innerHTML = forecastHtml;
 }
 function addTemperature(response){
     let temperature = document.querySelector("#temp");
